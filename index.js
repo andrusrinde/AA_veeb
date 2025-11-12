c377bf1d1e31aef33aebc2a4a89cdca48d766ba9
Andrus Rinde <andrusrinde@users.noreply.github.com>
2025-11-12 10:16:44 +0200

    Kodutöö vesimärgi lisamise ja viimase avaliku pildi näitamisega, tehtud

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const bodyparser = require("body-parser");
-//const mysql = require("mysql2/promise");
+const mysql = require("mysql2/promise");
 const dateEt = require("./src/dateTimeET");
-//const dbInfo = require("../../../../vp2025config");
+const dbInfo = require("../../../../vp2025config");
 const textRef = "public/txt/vanasonad.txt";
 const app = express();
 app.set("view engine", "ejs");
@@ -11,8 +11,36 @@ app.use(express.static("public"));
 //kui vormist tuleb vaid tekst, siis false, kui muud ka, siis true
 app.use(bodyparser.urlencoded({extended: true}));
 
-app.get("/", (req, res)=>{
-	res.render("index");
+const dbConf = {
+	host: dbInfo.configData.host,
+	user: dbInfo.configData.user,
+	password: dbInfo.configData.passWord,
+	database: dbInfo.configData.dataBase
+};
+
+app.get("/", async (req, res)=>{
+	let conn;
+	try {
+		conn = await mysql.createConnection(dbConf);
+		let sqlReq = "SELECT filename, alttext FROM galleryphotos_aa WHERE id=(SELECT MAX(id) FROM galleryphotos_aa WHERE privacy=? AND deleted IS NULL)";
+		const privacy = 3;
+		const [rows, fields] = await conn.execute(sqlReq, [privacy]);
+		console.log(rows);
+		let imgAlt = "Avalik foto";
+		if(rows[0].alttext != ""){
+			imgAlt = rows[0].alttext;
+		}
+		res.render("index", {imgFile: "gallery/normal/" + rows[0].filename, imgAlt: imgAlt});
+	}
+	catch(err){
+		res.render("index");
+	}
+	finally {
+		if(conn){
+			await conn.end();
+			console.log("Andmebaasiühendus suletud!");
+		}
+	}
 });
 
 app.get("/timenow", (req, res)=>{
